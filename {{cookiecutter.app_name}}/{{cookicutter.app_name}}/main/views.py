08d5194a778ee5a1c432ef8a7ea0cde8570c5895
Simon Larsen <slarse@kth.se>
2017-06-29 17:55:57 +0200

    Add properly nested project structure

---
+++ {{cookiecutter.app_name}}/{{cookicutter.app_name}}/main/views.py
@@ -0,0 +1,15 @@
+"""Views for the main package of {cookiecutter.app_name}
+
+.. module:: views
+    :platform: Unix
+    :synopsis: Views for the main package.
+.. moduleauthor:: {cookiecutter.author_name} <{cookiecutter.email}>
+"""
+from flask import render_template
+from . import main
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+
